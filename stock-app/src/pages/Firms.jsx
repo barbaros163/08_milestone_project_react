3c6003af0e5db9e9d80d45cc707b7848c59a67e7
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-20 18:09:55 +0100

    thunder doesn't work must be repaired by backend

--- stock-app/src/pages/Firms.jsx
+++ stock-app/src/pages/Firms.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const Firms = () => {
+  const { token } = useSelector((state) => state.auth);
+  const BASE_URL = "https://14173.fullstack.clarusway.com/";
+  const getFirms = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+
   return <div>Firms</div>;
 };
 
