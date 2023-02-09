8a0f5ea6606320205765667a3d61e46f974ac1f5
Yasin Yavuz <barbaros1675@gmail.com>
2023-02-09 12:16:30 +0100

    sca

--- stock-app/src/components/FirmCard.jsx
+++ stock-app/src/components/FirmCard.jsx
@@ -6,7 +6,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
-import { btnHoverStyle, flexCenter, flexColumn } from "../styles/globalStyle";
+import { btnHoverStyle, flex } from "../styles/globalStyle";
 import useStockCalls from "../hooks/useStockCalls";
 import { CardHeader } from "@mui/material";
 
@@ -17,19 +17,16 @@ export default function FirmCard({ firm, setOpen, setInfo }) {
       elevation={10}
       sx={{
         p: 2,
-        maxWidth: "300px",
-        maxHeight: "400px",
-        minHeight: "400px",
+        width: "300px",
+        height: "400px",
         display: "flex",
         flexDirection: "column",
       }}
     >
       <CardHeader title={firm?.name} subheader={firm?.address} />
       <CardMedia
-        height="325"
-        width="250"
         image={firm?.image}
-        sx={{ p: 1, objectFit: "contain" }}
+        sx={{ p: 1, objectFit: "contain", height: "130px" }}
         component="img"
         alt="firm-img"
       />
@@ -38,7 +35,7 @@ export default function FirmCard({ firm, setOpen, setInfo }) {
           Phone: {firm?.phone}
         </Typography>
       </CardContent>
-      <CardActions sx={flexCenter}>
+      <CardActions sx={flex}>
         <EditIcon
           sx={btnHoverStyle}
           onClick={() => {
--- stock-app/src/styles/globalStyle.js
+++ stock-app/src/styles/globalStyle.js
@@ -15,20 +15,29 @@ export const modalStyle = {
 };
 export const flexCenter = {
   display: "flex",
+  flexDirection: { xs: "column", sm: "row" },
   justifyContent: "center",
+  alignItems: "center",
   gap: 2,
+};
+
+export const flex = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: 1,
 };
 
 export const flexColumn = {
   display: "flex",
   flexDirection: "column",
-  justifyContent: "center",
   gap: 2,
 };
 
 export const arrowStyle = {
   display: "flex",
   justifyContent: "center",
+  alignItems: "center",
   cursor: "pointer",
   "&:hover": { color: "red" },
 };
